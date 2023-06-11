4209f9c974aba277d82741607fb64204f3290c71
Bilal Mirza <bsohail420@gmail.com>
2023-06-12 02:07:11 +0500

    Just created 'main.js' and 'CurrentLocation.js'.

---
+++ dist/js/CurrentLocation.js
@@ -0,0 +1,41 @@
+
+export default class CurrentLocation {
+    constructor() {
+        this._name = "Current Location";
+        this._lat = null;
+        this._lon = null;
+        this._unit = "imperial";
+    }
+
+    setName(name) {
+        this._name = name;
+    }
+    getName() {
+        return this._name;
+    }
+
+    setLat(lat) {
+        this._lat = lat;
+    }
+    getLat() {
+        return this._lat;
+    }
+
+    setLon(lon) {
+        this._lon = lon;
+    }
+    getLon() {
+        return this._lon;
+    }
+
+    setUnit(unit) {
+        this._unit = unit;
+    }
+    getUnit() {
+        return this._unit;
+    }
+
+    toggleUnit() {
+        this._unit = this._unit === "imperial" ? "metric" : "imperial";
+    }
+}
---
+++ dist/js/main.js
@@ -0,0 +1,16 @@
+
+import CurrentLocation from "./CurrentLocation.js";
+const currentLoc = new CurrentLocation();
+
+const intitApp = () =>  {
+    // add listners 
+
+    const geoButton = document.getElementById("getLocation ")
+
+    // set up
+
+
+    // load weather
+}
+
+document.addEventListener("DOMContentLoaded", intitApp);
